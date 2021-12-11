9b5c058fb0da013cfc7a68756161a9af53cf78a3
oscarvictoria <oscarvictoriagonzalez@gmail.com>
2021-12-10 20:59:13 -0500

    added birth year variable to calculate the age of the user

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const readlineSync = require("readline-sync")
-const name = readlineSync.question("What's your name? \n")
-console.log("Hi " + name + " nice to meet you! \n")
+const readlineSync = require("readline-sync");
+const name = readlineSync.question("What's your name? \n");
+console.log("Hi " + name + " nice to meet you! \n");
 
 // Part 2: Age 
 
-const age = readlineSync.question('What is you age? \n')
-console.log('Looks like your age is ' + age)
+const birthYear = readlineSync.question('What year were you born? \n');
+let age = 2021 - birthYear;
+console.log(`Looks like your age is ${age}, is this correct?`);
+
+// Part 3: Confirm age
+const confirm = readlineSync.question("\n");
+console.log('Great, lets continue...');
