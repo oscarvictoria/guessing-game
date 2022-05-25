1097099791374734a5840c9f61592c0fc51d31c5
oscarvictoria <oscarvictoriagonzalez@gmail.com>
2022-05-24 22:46:09 -0400

    updated year

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ console.log("Hi " + capitalizedName + " nice to meet you! \n");
 // Part 2: Age 
 
 const birthYear = readlineSync.question('What year were you born? \n');
-let age = 2021 - birthYear;
+let age = 2022 - birthYear;
 console.log(`Looks like your age is ${age}, is this correct?\n`);
 
 // Part 3: Confirm age
 const confirm = readlineSync.keyInYN('Y or N');
 console.log('Great...');
+
